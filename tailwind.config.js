5d5b878cec0525c048f8a1205a63d3434bf67e4d
Toni <35571969+findtoni@users.noreply.github.com>
2022-08-08 14:17:38 +0100

    chore: setup deps * added hardhat
    * added vue3, pinia, ts
    
    * added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {}
+      },
+      colors: {},
+      fontFamily: {}
+    },
+  },
+  plugins: [],
+}
