752777a88f2e4847a90c5fbb70c479aa3a716139
Toni <35571969+findtoni@users.noreply.github.com>
2022-10-14 10:39:17 +0100

    chore: clear repo

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import './assets/styles/tailwind.css';
-import './assets/global.scss';
-
-createApp(App)
-  .use(createPinia())
-  .mount('#app')
