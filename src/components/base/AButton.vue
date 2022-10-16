babde17c10c5267e6dc5e13d57f992ed2b646d36
Toni <35571969+findtoni@users.noreply.github.com>
2022-10-16 05:01:29 +0100

    feat: added base components

---
+++ src/components/base/AButton.vue
@@ -0,0 +1,37 @@
+<script setup>
+import ALoading from '@/components/base/ALoading.vue';
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: Function,
+    required: false,
+  },
+  loading: {
+    type: Boolean,
+    required: false,
+  },
+  disabled: {
+    type: Boolean,
+    required: false,
+  }
+});
+</script>
+
+<template>
+  <button v-if="props.loading" class="w-3/4 md:w-2/4 h-10 rounded bg-gray-200 text-gray-500 cursor-not-allowed shadow-sm">
+    <ALoading class="mb-0.5 mr-1" />
+    Fetching...
+  </button>
+  <button v-else-if="props.disabled" class="w-3/4 md:w-2/4 h-10 rounded bg-gray-200 text-gray-500 cursor-not-allowed shadow-sm">
+    <component :is="props.icon" class="w-5 h-5 mb-1 mr-0.5 inline-flex text-gray-500" />
+    {{ props.label }}
+  </button>
+  <button v-else class="w-3/4 md:w-2/4 h-10 rounded bg-emerald-600 text-white font-medium cursor-pointer shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2">
+    <component :is="props.icon" class="w-5 h-5 mb-1 mr-0.5 inline-flex text-white" />
+    {{ props.label }}
+  </button>
+</template>
---
+++ src/components/base/ACheckbox.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+
+const toggle = ref(false);
+const props = defineProps({
+  name: {
+    type: String,
+    required: false,
+  },
+  label: {
+    type: String,
+    required: false,
+  },
+  loading: {
+    type: Boolean,
+    required: false,
+  },
+});
+defineEmits(['change']);
+</script>
+
+<template>
+  <div class="relative flex items-start">
+    <div class="flex h-5 items-center">
+      <input
+        v-model="toggle"
+        :id="props.name"
+        :name="props.name"
+        :disabled="props.loading"
+        type="checkbox"
+        @change="$emit('change', toggle)"
+        class="h-4 w-4 rounded border-gray-300 accent-pink-600 text-pink-600 focus:ring-pink-500" />
+    </div>
+    <div class="ml-2 text-sm">
+      <label :for="props.name" class="font-medium text-white">
+        {{ props.label }}</label
+      >
+    </div>
+  </div>
+</template>
