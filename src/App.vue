bf4024689bfca3957891963394ac37d5e0aedcc0
Toni <35571969+findtoni@users.noreply.github.com>
2022-10-16 05:03:13 +0100

    feat: added app layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-<script lang="ts" setup>
+<script setup>
+import { useStore } from '@/store';
+import ALogo from '@/components/ALogo.vue';
+import ASearch from '@/components/ASearch.vue';
+import AGallery from '@/components/AGallery.vue';
+import AFooter from '@/components/AFooter.vue';
 
+const store = useStore();
 </script>
 
 <template>
-
+  <div class="bg-slate-800 h-full w-full">
+    <div class="flex flex-col items-center justify-center h-screen">
+      <ALogo />
+      <ASearch />
+      <AGallery v-if="store.nfts.length >= 1" />
+    </div>
+    <AFooter />
+  </div>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
